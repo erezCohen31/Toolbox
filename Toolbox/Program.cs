9bf0e3299745191f8b781562cdf1776f08084591
erez cohen <erezjbh@gmail.com>
2025-05-19 12:09:54 +0300

    add wirestripper

--- Toolbox/Program.cs
+++ Toolbox/Program.cs
@@ -10,14 +10,17 @@ public class program
         BaseTool hammer = new Hammer("Hammer", 1.5);
         BaseTool screwdriver = new Screwdriver("Screwdriver", 0.5);
         BaseTool wrench = new Wrench("Wrench", 2.0);
+        BaseTool wirestrip = new WireStripper("WireStripper", 0.5);
         // Add tools to the toolbox
         myToolBox.AddTool(hammer);
         myToolBox.AddTool(screwdriver);
         myToolBox.AddTool(wrench);
+        myToolBox.AddTool(wirestrip);
         // List tools in the toolbox
         myToolBox.ListTools();
         // Use a tool
         myToolBox.UseTool(hammer);
+        myToolBox.UseTool(screwdriver);
         // Remove a tool from the toolbox
         myToolBox.RemoveTool(screwdriver);
         // List tools again
---
+++ Toolbox/WireStripper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Toolbox
+{
+    public class WireStripper : BaseTool
+    {
+        public static int count = 0;
+        public WireStripper(string name, double weight) : base(name, weight)
+        {
+        }
+        public override void Describe()
+        {
+            Console.WriteLine("This is a wire stripper.");
+        }
+        public override void Use()
+        {
+            Console.WriteLine("The wire stripper is being used.");
+            count++;
+        }
+    }
+    
+    
+}
